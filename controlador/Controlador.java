4bb2932a2f49945e0fe23c9883911b7adce6c0a6
daniu16 <cesar2224642@correo.uis.edu.co>
2023-04-29 09:59:42 -0500

    dadadadaddaaaaa

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -1,1 +1,25 @@
 package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import vista.VentanaPrincipal;
+
+public class Controlador implements ActionListener{
+
+    public Controlador(VentanaPrincipal vp){
+
+        this.vP = vP;
+
+        @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        String event = e.getActionCommand();
+
+
+        if(event == "salir")
+        {
+            System.exit(0);
+        }  
+    }
+}
